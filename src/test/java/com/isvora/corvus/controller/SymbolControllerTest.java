9bd8fe231b3e5259e28c7b313f2861d8c03e4c3c
isvora <isvoranandrei25@gmail.com>
2022-08-29 17:50:12 +0300

    controller test

--- src/test/java/com/isvora/corvus/controller/SymbolControllerTest.java
+++ src/test/java/com/isvora/corvus/controller/SymbolControllerTest.java
@@ -5,7 +5,6 @@ import com.isvora.corvus.service.StockTwitsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
